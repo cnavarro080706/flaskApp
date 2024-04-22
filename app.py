0343be5f178055c921f690e423dab4f660cf8edd
Chris Navarro <ejnavarro06@gmail.com>
2024-04-22 23:15:52 +0800

    first commit

---
+++ app.py
@@ -0,0 +1,199 @@
+from functools import wraps
+from flask import Flask, render_template, flash, redirect, url_for, session, request,logging                                                        # Importing the Flask module
+from data import Articles                                      # Importing the articles data
+from flask_mysqldb import MySQL                                # Importing MySQL flask module
+from wtforms import Form, StringField, TextAreaField, PasswordField, validators
+from passlib.hash import sha256_crypt                          # Import module to encrypt passwords
+
+app = Flask(__name__)                                          # Flask Object
+
+# Config MySQL
+app.config["MYSQL_HOST"] = "localhost"
+app.config["MYSQL_USER"] = "root"
+app.config["MYSQL_PASSWORD"] = "devpasswd"
+app.config["MYSQL_DB"] = "myflaskapp"
+app.config["MYSQL_CURSORCLASS"] = "DictCursor"                 # Coverting received data to Dict
+
+# initialize MySQL
+mysql = MySQL(app)
+
+app.debug=True                                                 # enable this for page refresh
+
+Articles = Articles()
+
+# Index
+@app.route("/")                                                # route for index page
+def index():
+    return render_template("index.html")                       # create a sample view function
+# About
+@app.route("/about")
+def about():
+    return render_template("about.html")
+
+# Articles
+@app.route("/articles")
+def articles():
+    return render_template("articles.html", articles = Articles)
+
+# Single Article
+@app.route("/article/<string:id>/")
+def article(id):
+    return render_template("article.html", id=id)
+
+# Register Form Class
+class RegistrationForm(Form):
+    lastname = StringField("Lastname", [validators.Length(min=1, max=50)])
+    firstname = StringField("Firstname", [validators.Length(min=1, max=50)])
+    username = StringField("Username", [validators.Length(min=4, max=25)])
+    email = StringField("Email", [validators.Length(min=6, max=50)])
+    password = PasswordField("Password", [
+        validators.DataRequired(),
+        validators.EqualTo("confirm", message="Passwords do not match!")
+        ])
+    confirm = PasswordField("Confirm Password")
+
+# User Registeration
+@app.route("/register", methods=["GET", "POST"])
+def register():
+    form = RegistrationForm(request.form)
+    if request.method == "POST" and form.validate():
+        lastname = form.lastname.data
+        firstname = form.firstname.data
+        username = form.username.data
+        email = form.email.data
+        password = sha256_crypt.encrypt(str(form.password.data))        # encrypt the plain text pw
+
+        # create cursor
+        cur = mysql.connection.cursor()
+
+        # execute query
+        cur.execute("INSERT INTO users (lastname, firstname, username, email, password) VALUES (%s, %s, %s, %s, %s)", (lastname, firstname, username, email, password))
+
+        # commit to DB
+        mysql.connection.commit()
+
+        # Close connection
+        cur.close()
+
+        flash("Your are Registered Successfully!", "success")
+
+        return redirect(url_for("index")) 
+    return render_template("register.html", form=form)   
+
+# User login
+@app.route("/login", methods=["GET", "POST"])
+def login():
+    if request.method == "POST":
+        # Get form fields
+        username = request.form["username"]
+        password_candidate =  request.form["password"]
+
+        # Create cursor
+        cur = mysql.connection.cursor()
+       
+        # Execute Get Query by username
+        result = cur.execute("SELECT * FROM users WHERE username = %s", [username])
+
+        if result > 0:
+            # Get MySQL PW hash
+            data = cur.fetchone()
+            password = data["password"]
+
+            # Compare user login password to MySQL user password
+            if sha256_crypt.verify(password_candidate, password):
+                # app.logger.info("PASSWORD MATCHED.") - This is for debugging purposes
+                # Cature session variables
+                session["logged_in"] = True
+                session["username"] = username
+
+                flash('You have logged in successfuly!', "success")
+                return redirect(url_for("dashboard"))
+            else:
+                # app.logger.info("PASSWORD NOT MATCHED!") - This is for debugging purposes
+                error = "Invalid Username or Password!"
+                return render_template("login.html", error=error)
+            # Close connection
+            cur.close()
+        else:
+            # app.logger.info("NO USER!") - This is for debugging purposes
+            error = "Username not found."
+            return render_template("login.html", error=error)
+    
+    return render_template("login.html")
+
+# Check if the user logged in
+def is_logged_in(f):
+    @wraps(f)
+    def wrap(*args, **kwargs):
+        if "logged_in" in session:
+            return f(*args, ** kwargs)
+        else:
+            flash("Unauthorized, Please login.", "danger")
+            return redirect(url_for("login"))
+    return wrap
+
+# Logout
+@app.route('/logout')
+@is_logged_in
+def logout():
+    session.clear()
+    flash("You're Session was logged out sucessfully!", "success")
+    return redirect(url_for("login"))
+
+# Dashboard
+@app.route("/dashboard")
+@is_logged_in
+def dashboard():
+    # Create Cursor
+    cur = mysql.connection.cursor()
+
+    # Get articles
+    result = cur.execute("SELECT * FROM articles")
+
+    articles = cur.fetchall()
+
+    if result > 0:
+        return render_template("dashboard.html", articles=articles)
+    else:
+        msg = "No articles found."
+        return render_template("dashboard.html", msg=msg)
+
+    # Close connection
+    cur.close()
+    
+# Article Form Class
+class ArticleForm(Form):
+    title = StringField("Title", [validators.Length(min=1, max=200)])
+    body = TextAreaField("Body", [validators.Length(min=1)])
+   
+# Add Article
+@app.route("/add_article", methods=["GET", "POST"])
+@is_logged_in
+def add_article():
+    form = ArticleForm(request.form)
+    if request.method == "POST" and form.validate():
+        title = form.title.data
+        body = form.body.data
+
+        # Create Cursor
+
+        cur = mysql.connection.cursor()
+
+        # Execute CRUD
+        cur.execute("INSERT INTO articles(title, body, author) VALUES (%s, %s, %s)", (title, body, session["username"]))
+
+        # Commit to DB
+        mysql.connection.commit()
+
+        # Close connection
+        cur.close()
+        flash(" Article created succesfully!", "success")
+        return redirect(url_for("dashboard"))
+   
+    return render_template("add_article.html", form=form)
+
+if __name__ == "__main__":                                     # Run the application in  main
+    app.secret_key="secret_1234"
+    app.run()
+
+
---
+++ data.py
@@ -0,0 +1,26 @@
+def Articles():
+    articles = [
+        {
+            "id": 1,
+            "title": "Article One",
+            "body": "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam. ",
+            "author": "Chris Navarro",
+            "create_date": "04-19-2024"
+        },
+          {
+            "id": 2,
+            "title": "Article Two",
+            "body": "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam. ",
+            "author": "Ej Baligod",
+            "create_date": "04-19-2024"
+        },
+          {
+            "id": 3,
+            "title": "Article Three",
+            "body": "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam. ",
+            "author": "Gaud Tywin Miras",
+            "create_date": "04-19-2024"
+        },
+    ]
+
+    return articles
