8bb1d037585acb9944f49b50411bcd84dcd36b65
Chris Navarro <ejnavarro06@gmail.com>
2024-04-23 22:55:23 +0800

    Updated files and added some functionality

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
 from functools import wraps
 from flask import Flask, render_template, flash, redirect, url_for, session, request,logging                                                        # Importing the Flask module
-from data import Articles                                      # Importing the articles data
+# from data import Articles                                      # Importing the articles data
 from flask_mysqldb import MySQL                                # Importing MySQL flask module
 from wtforms import Form, StringField, TextAreaField, PasswordField, validators
 from passlib.hash import sha256_crypt                          # Import module to encrypt passwords
@@ -19,7 +19,7 @@ mysql = MySQL(app)
 
 app.debug=True                                                 # enable this for page refresh
 
-Articles = Articles()
+# Articles = Articles()
 
 # Index
 @app.route("/")                                                # route for index page
@@ -33,12 +33,35 @@ def about():
 # Articles
 @app.route("/articles")
 def articles():
-    return render_template("articles.html", articles = Articles)
+    # Create Cursor
+    cur = mysql.connection.cursor()
+
+    # Get articles
+    result = cur.execute("SELECT * FROM articles")
+
+    articles = cur.fetchall()
+
+    if result > 0:
+        return render_template("articles.html", articles=articles)
+    else:
+        msg = "No articles found."
+        return render_template("articles.html", msg=msg)
+
+    # Close connection
+    cur.close()
 
 # Single Article
 @app.route("/article/<string:id>/")
 def article(id):
-    return render_template("article.html", id=id)
+    # Create Cursor
+    cur = mysql.connection.cursor()
+
+    # Get article
+    result = cur.execute("SELECT * FROM articles WHERE id = %s", [id])
+
+    article = cur.fetchone()
+
+    return render_template("article.html", article=article)
 
 # Register Form Class
 class RegistrationForm(Form):
@@ -191,6 +214,65 @@ def add_article():
         return redirect(url_for("dashboard"))
    
     return render_template("add_article.html", form=form)
+
+# Edit Article
+@app.route("/edit_article/<string:id>", methods=["GET", "POST"])
+@is_logged_in
+def edit_article(id):
+
+    # Create cursor
+    cur = mysql.connection.cursor()
+
+    # Get article by id
+    result = cur.execute("SELECT * FROM articles WHERE id = %s", [id])
+    article = cur.fetchone()
+
+    # Get form
+    form = ArticleForm(request.form)
+
+    # Populate article form fields
+    form.title.data = article["title"]
+    form.body.data = article["body"]
+
+    if request.method == "POST" and form.validate():
+        title = request.form["title"]
+        body = request.form["body"]
+
+        # Create Cursor
+
+        cur = mysql.connection.cursor()
+
+        # Execute CRUD
+        cur.execute("UPDATE articles SET title=%s, body=%s WHERE id=%s", (title, body, id))
+
+        # Commit to DB
+        mysql.connection.commit()
+
+        # Close connection
+        cur.close()
+        flash(" Article updated succesfully!", "success")
+        return redirect(url_for("dashboard"))
+   
+    return render_template("edit_article.html", form=form)
+
+# Delete Article
+@app.route("/delete_article/<string:id>", methods=["POST"])
+@is_logged_in
+def delete_article(id):
+    # Create cursor
+    cur = mysql.connection.cursor()
+
+    # Execute
+    cur.execute("DELETE FROM articles WHERE id=%s", [id])
+
+    # Commit to DB
+    mysql.connection.commit()
+
+    # Close connection
+    cur.close()
+
+    flash(" Article deleted succesfully!", "success")
+    return redirect(url_for("dashboard"))
 
 if __name__ == "__main__":                                     # Run the application in  main
     app.secret_key="secret_1234"
